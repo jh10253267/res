6f04501dfb3ff4f95872a7a7d9ce26d6d87c6338
Gwon JunHyeok <jh10253267@gmail.com>
2025-03-07 00:23:43 +0900

    feature(#3): room 컨트롤러 작성
    기본 작성

--- src/main/java/com/studioreservation/domain/room/dto/RoomRequestDTO.java
+++ src/main/java/com/studioreservation/domain/room/dto/RoomRequestDTO.java
@@ -1,19 +1,15 @@
 package com.studioreservation.domain.room.dto;
 
-import java.sql.Timestamp;
-
 import lombok.Builder;
 import lombok.Getter;
-import lombok.ToString;
 
 @Getter
 @Builder
-@ToString
 public class RoomRequestDTO {
 	private String name;
 	private String hrPrice;
 	private String dayPrice;
-	private Timestamp minTm;
+	private int minTm;
 	private int capacity;
 	private String title;
 	private String description;
