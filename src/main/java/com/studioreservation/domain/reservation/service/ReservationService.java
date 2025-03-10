8c3c7f13f88c6bf4dadba09777d1e32035aafe23
Gwon JunHyeok <jh10253267@gmail.com>
2025-03-10 10:08:50 +0900

    feature(#3): 예약 서비스 구현
    추후 JOOQ 도입을 염두해두고 있어서 임시 코드 작성

--- src/main/java/com/studioreservation/domain/reservation/service/ReservationService.java
+++ src/main/java/com/studioreservation/domain/reservation/service/ReservationService.java
@@ -1,17 +1,23 @@
 package com.studioreservation.domain.reservation.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
-import com.studioreservation.domain.reservation.dto.ReservationDTO;
+import com.studioreservation.domain.reservation.dto.ReservationResponseDTO;
 import com.studioreservation.domain.reservation.dto.ReservationRequestDTO;
 import com.studioreservation.domain.reservation.entity.ReservationHistory;
 import com.studioreservation.domain.reservation.mapper.ReservationMapper;
 import com.studioreservation.domain.reservation.repository.ReservationRepository;
 import com.studioreservation.domain.room.entity.Room;
 import com.studioreservation.domain.room.repository.RoomRepository;
+import com.studioreservation.global.request.PageRequestDTO;
+import com.studioreservation.global.response.PageResponseDTO;
 
 import lombok.RequiredArgsConstructor;
 
@@ -22,16 +28,30 @@ public class ReservationService {
 	private final RoomRepository roomRepository;
 	private final ReservationMapper reservationMapper;
 
-	public List<ReservationDTO> getAllReservation() {
-		return reservationRepository.findAll().stream()
-			.map(entity -> reservationMapper.toDTO(entity))
+	public List<ReservationResponseDTO> getAllReservation() {
+		return reservationRepository.findAll()
+			.stream()
+			.map(reservationMapper::toDTO)
 			.toList();
 	}
 
-	public void reserve(Long roomCd, ReservationRequestDTO reservationRequestDTO) {
+	public PageResponseDTO<ReservationResponseDTO> getPagedReservations(PageRequestDTO pageRequestDTO) {
+		Page<ReservationResponseDTO> result = reservationRepository
+			.findAll(pageRequestDTO.getPageable(pageRequestDTO.getSortBy()))
+			.map(entity -> reservationMapper.toDTO(entity));
+
+		return PageResponseDTO.<ReservationResponseDTO>withAll()
+			.data(result.getContent())
+			.pageRequestDTO(pageRequestDTO)
+			.total(result.getTotalElements())
+			.build();
+	}
+
+	public ReservationResponseDTO reserve(Long roomCd, ReservationRequestDTO reservationRequestDTO) {
 		ReservationHistory reservationHistory = reservationMapper.toEntity(reservationRequestDTO);
 		Room room = roomRepository.findSingleEntity(roomCd);
 		reservationHistory.setRoom(room);
-		reservationRepository.save(reservationHistory);
+
+		return reservationMapper.toDTO(reservationRepository.save(reservationHistory));
 	}
 }
