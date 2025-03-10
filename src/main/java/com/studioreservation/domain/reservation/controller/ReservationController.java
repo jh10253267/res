c5b39e2af08239642becc34a0d4d66f20d7ebd03
Gwon JunHyeok <jh10253267@gmail.com>
2025-03-10 10:04:39 +0900

    feature(#3): 기본 컨트롤러 구현
    페이징유무에 따라 메소드를 나누어 처리

--- src/main/java/com/studioreservation/domain/reservation/controller/ReservationController.java
+++ src/main/java/com/studioreservation/domain/reservation/controller/ReservationController.java
@@ -1,16 +1,21 @@
 package com.studioreservation.domain.reservation.controller;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.studioreservation.domain.reservation.dto.ReservationRequestDTO;
+import com.studioreservation.domain.reservation.dto.ResvPageRequestDTO;
 import com.studioreservation.domain.reservation.service.ReservationService;
+import com.studioreservation.global.request.PageRequestDTO;
 import com.studioreservation.global.response.ApiResponse;
 
+import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -19,9 +24,15 @@ import lombok.RequiredArgsConstructor;
 public class ReservationController {
 	private final ReservationService reservationService;
 
+
 	@GetMapping
 	public ApiResponse<?> getAllReservation() {
 		return ApiResponse.success(reservationService.getAllReservation());
+	}
+
+	@GetMapping
+	public ApiResponse<?> getAllReservation(PageRequestDTO pageRequestDTO) {
+		return ApiResponse.success(reservationService.getPagedReservations(pageRequestDTO));
 	}
 
 	@PostMapping("/{roomCd}")
