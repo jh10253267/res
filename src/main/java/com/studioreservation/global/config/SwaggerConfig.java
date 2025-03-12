97af7b221c0c5b179d4bfdac6456e028e28d9fa3
Gwon JunHyeok <jh10253267@gmail.com>
2025-03-13 08:16:10 +0900

    feature(#5): 스웨거 컨피그 토큰이름 추가
    보안을 추가하여 토큰을 처리할 수 있도록 애노테이션 추가

--- src/main/java/com/studioreservation/global/config/SwaggerConfig.java
+++ src/main/java/com/studioreservation/global/config/SwaggerConfig.java
@@ -4,6 +4,22 @@ import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+
+@OpenAPIDefinition(
+	info = @Info(title = "Studio Reservation",
+		description = "Studio Reservation Service",
+		version = "v1"))
+
+@SecurityScheme(
+	name = "Bearer Authentication",
+	type = SecuritySchemeType.HTTP,
+	bearerFormat = "JWT",
+	scheme = "bearer"
+)
 
 @Configuration
 public class SwaggerConfig {
@@ -11,7 +27,7 @@ public class SwaggerConfig {
 	public GroupedOpenApi publicOpenApi() {
 		return GroupedOpenApi.builder()
 			.group("Reservation")
-			.pathsToMatch("/api/**")
+			.pathsToMatch("/**")
 			.build();
 	}
 }
