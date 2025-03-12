43a29cb9e280f087b25923dc101e524dbf44f1de
Gwon JunHyeok <jh10253267@gmail.com>
2025-03-13 08:21:18 +0900

    feature(#5): 스웨거 토큰 애노테이션 추가

--- src/main/java/com/studioreservation/domain/room/controller/RoomController.java
+++ src/main/java/com/studioreservation/domain/room/controller/RoomController.java
@@ -1,5 +1,6 @@
 package com.studioreservation.domain.room.controller;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,10 +10,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.studioreservation.global.response.ApiResponse;
+import com.studioreservation.global.response.APIResponse;
 import com.studioreservation.domain.room.dto.RoomRequestDTO;
 import com.studioreservation.domain.room.service.RoomService;
 
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -22,24 +24,28 @@ public class RoomController {
 	private final RoomService roomService;
 
 	@GetMapping
-	public ApiResponse<?> getAllRoom() {
-		return ApiResponse.success(roomService.getAllRoom());
+	public APIResponse<?> getAllRoom() {
+		return APIResponse.success(roomService.getAllRoom());
 	}
 
 	@GetMapping("/{roomCd}")
-	public ApiResponse<?> getRoom(@PathVariable("roomCd") Long roomCd,
+	public APIResponse<?> getRoom(@PathVariable("roomCd") Long roomCd,
 		@RequestParam(required = true) int ymd) {
 		System.out.println(ymd);
-		return ApiResponse.success(roomService.getRoom(roomCd));
+		return APIResponse.success(roomService.getRoom(roomCd));
 	}
 
+	@SecurityRequirement(name = "Bearer Authentication")
 	@PostMapping
-	public void makeRoom(@RequestBody RoomRequestDTO roomRequestDTO) {
+	public void makeRoom(@RequestBody RoomRequestDTO roomRequestDTO, Authentication authentication) {
+		// 이런식으로 유저의 이름과 권한을 뽑아내기
+		System.out.println(authentication.getPrincipal().toString());
 		roomService.createRoom(roomRequestDTO);
 	}
 
+	@SecurityRequirement(name = "Bearer Authentication")
 	@PatchMapping("/{roomCd}")
-	public ApiResponse<?> editRoom(@PathVariable("roomCd") Long roomCd,
+	public APIResponse<?> editRoom(@PathVariable("roomCd") Long roomCd,
 		@RequestBody RoomRequestDTO roomRequestDTO) {
 		roomService.editRoom(roomCd, roomRequestDTO);
 		return null;
