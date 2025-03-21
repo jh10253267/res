51e936a6bbba4bdb510623a55ccb0d14e3756be5
Gwon JunHyeok <jh10253267@gmail.com>
2025-03-21 20:51:25 +0900

    feature(#6): 토큰 체크 롤백
    토큰 체크시 의도한대로 동작하지 않는 문제를 발견하고 롤백

--- src/main/java/com/studioreservation/global/security/filter/TokenCheckFilter.java
+++ src/main/java/com/studioreservation/global/security/filter/TokenCheckFilter.java
@@ -2,11 +2,9 @@ package com.studioreservation.global.security.filter;
 
 import java.io.IOException;
 import java.util.Map;
-import java.util.Set;
 
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -20,28 +18,20 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
+@RequiredArgsConstructor
 public class TokenCheckFilter extends OncePerRequestFilter {
 	private final JWTUtil jwtUtil;
-	private final Map<String, Set<String>> publicPaths;
-
-	public TokenCheckFilter(JWTUtil jwtUtil) {
-		this.jwtUtil = jwtUtil;
-		this.publicPaths = Map.of(
-			"/login", Set.of("POST"),
-			"/api/signup", Set.of("POST"),
-			"/api/rooms", Set.of("GET"));
-	}
 
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
 		FilterChain filterChain) throws ServletException, IOException {
-		log.info("!!!!");
-		System.out.println(SecurityContextHolder.getContext());
+		String path = request.getRequestURI();
 
-		if (isPublicPath(request.getRequestURI(), request.getMethod())) {
+		if (!path.startsWith("/api/")) {
 			filterChain.doFilter(request, response);
 			return;
 		}
@@ -57,11 +47,6 @@ public class TokenCheckFilter extends OncePerRequestFilter {
 		} catch (AccessTokenException e) {
 			e.sendResponseError(response);
 		}
-	}
-
-	private boolean isPublicPath(String requestUri, String method) {
-		Set<String> allowedMethods = publicPaths.get(requestUri);
-		return allowedMethods != null && allowedMethods.contains(method);
 	}
 
 	private Map<String, Object> validateAccessToken(HttpServletRequest request) throws AccessTokenException {
