1be741bad919d5ac23a8840a8c6e27439e411a26
Gwon JunHyeok <jh10253267@gmail.com>
2025-06-02 10:05:31 +0900

    feature(#35): queryds로 사용

--- src/main/java/com/studioreservation/domain/reservation/repository/ReservationRepository.java
+++ src/main/java/com/studioreservation/domain/reservation/repository/ReservationRepository.java
@@ -7,13 +7,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import com.studioreservation.domain.reservation.entity.ReservationHistory;
+import com.studioreservation.domain.reservation.repository.custom.ReservationRepositoryCustom;
 
-public interface ReservationRepository extends JpaRepository<ReservationHistory, Long>,
-	JpaSpecificationExecutor<ReservationHistory> {
+public interface ReservationRepository extends JpaRepository<ReservationHistory, Long>, ReservationRepositoryCustom {
 
 	Optional<ReservationHistory> findByPhoneAndResvCd(String phone, String resvCd);
+
 	default ReservationHistory findReservationHistory(String phone, String resvCd) {
 		return findByPhoneAndResvCd(phone, resvCd).orElseThrow(); // TODO 예외 처리 작성하기
 	}
+
 	boolean existsByResvCd(String resvCd);
 }
