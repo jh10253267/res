ff624f37a9d3c26253d072c542157c78d918cbcd
Gwon JunHyeok <jh10253267@gmail.com>
2025-06-02 10:20:07 +0900

    feature(#35): 빌드 오류 수정

--- src/main/java/com/studioreservation/domain/studiofile/service/StudioFileService.java
+++ src/main/java/com/studioreservation/domain/studiofile/service/StudioFileService.java
@@ -4,7 +4,6 @@ import org.springframework.web.multipart.MultipartFile;
 import com.studioreservation.domain.studiofile.repository.StudioFileRepository;
 
 import lombok.RequiredArgsConstructor;
-import software.amazon.awssdk.services.s3.S3Client;
 
 @RequiredArgsConstructor
 public class StudioFileService {
