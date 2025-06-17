2ce3a02c63b310e1f386088484434ac189327f1d
Gwon JunHyeok <jh10253267@gmail.com>
2025-06-17 17:58:18 +0900

    feature(#67): 기능과 상관없는 수정사항

--- src/main/java/com/studioreservation/domain/reservation/dto/ReservedTimeReqDTO.java
+++ src/main/java/com/studioreservation/domain/reservation/dto/ReservedTimeReqDTO.java
@@ -2,6 +2,7 @@ package com.studioreservation.domain.reservation.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.studioreservation.domain.reservation.enums.ReservationState;
 import com.studioreservation.global.formatter.TimestampDeserializer;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
@@ -14,7 +15,6 @@ import java.sql.Timestamp;
 @Setter
 @NoArgsConstructor
 public class ReservedTimeReqDTO {
-
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyyMMddHHmmss")
     @JsonDeserialize(using = TimestampDeserializer.class)
     @Schema(type = "string", example = "예약 시작 시각", description = "예약 시작 시각")
