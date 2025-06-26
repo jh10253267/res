ec1bb15d437dbb63614f777e7efbd50f149e2edf
Gwon JunHyeok <jh10253267@gmail.com>
2025-06-26 10:00:27 +0900

    feature(#70): 이름 수정

--- src/main/java/com/studioreservation/domain/platform/controller/PlatformController.java
+++ src/main/java/com/studioreservation/domain/platform/controller/PlatformController.java
@@ -3,8 +3,10 @@ package com.studioreservation.domain.platform.controller;
 import com.studioreservation.global.response.APIResponse;
 import com.studioreservation.domain.platform.dto.PlatformReqDTO;
 import com.studioreservation.domain.platform.service.PlatformService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -14,16 +16,20 @@ import org.springframework.web.bind.annotation.*;
 public class PlatformController {
     private final PlatformService service;
 
+    @PreAuthorize("hasRole('ADMIN')")
+    @Operation(summary = "플랫폼 수정", description = "픞랫폼 수정")
+    @PutMapping("/{cd}")
+    public APIResponse<?> updatePlatform(@RequestBody PlatformReqDTO platformReqDTO,
+                                       @PathVariable("cd") Long cd) {
+        service.updatePlatform(platformReqDTO, cd);
+
+        return APIResponse.success();
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @Operation(summary = "플랫폼 생성", description = "픞랫폼 생성")
     @PostMapping
     public APIResponse<?> makePlatform(@RequestBody PlatformReqDTO platformReqDTO) {
         return APIResponse.success(service.makePlatform(platformReqDTO));
-    }
-
-    @PutMapping("/{cd}")
-    public APIResponse<?> editPlatform(@RequestBody PlatformReqDTO platformReqDTO,
-                                       @PathVariable("cd") Long cd) {
-        service.editPlatform(platformReqDTO, cd);
-
-        return APIResponse.success();
     }
 }
