6fa934e08e8bc258389c12a358e33ccc10dbc304
Gwon JunHyeok <jh10253267@gmail.com>
2025-07-22 18:15:19 +0900

    feature(#87): 플랫폼 컨트롤러 수정

--- src/main/java/com/studioreservation/domain/platform/controller/PlatformController.java
+++ src/main/java/com/studioreservation/domain/platform/controller/PlatformController.java
@@ -1,35 +1,20 @@
 package com.studioreservation.domain.platform.controller;
 
-import com.studioreservation.global.response.APIResponse;
-import com.studioreservation.domain.platform.dto.PlatformReqDTO;
 import com.studioreservation.domain.platform.service.PlatformService;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import com.studioreservation.global.response.APIResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/admin/platform")
-@SecurityRequirement(name = "Bearer Authentication")
+@RequestMapping("/api/platforms")
 public class PlatformController {
     private final PlatformService service;
 
-    @PreAuthorize("hasRole('ADMIN')")
-    @Operation(summary = "플랫폼 수정", description = "픞랫폼 수정")
-    @PutMapping("/{cd}")
-    public APIResponse<?> updatePlatform(@RequestBody PlatformReqDTO platformReqDTO,
-                                       @PathVariable("cd") Long cd) {
-        service.updatePlatform(platformReqDTO, cd);
-
-        return APIResponse.success();
-    }
-
-    @PreAuthorize("hasRole('ADMIN')")
-    @Operation(summary = "플랫폼 생성", description = "픞랫폼 생성")
-    @PostMapping
-    public APIResponse<?> makePlatform(@RequestBody PlatformReqDTO platformReqDTO) {
-        return APIResponse.success(service.makePlatform(platformReqDTO));
+    @GetMapping
+    public APIResponse<?> getAllPlatforms() {
+        return APIResponse.success(service.getAllPlatform());
     }
 }
