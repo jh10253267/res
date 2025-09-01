c3f999eb9176141d3b6ebb35906e649ceab718e0
Gwon JunHyeok <jh10253267@gmail.com>
2025-09-01 20:06:33 +0900

    feature(#100): 테스트 코드 작성 및 수정
    테스트 모킹 중 불필요한 코드를 찾아 삭제

--- src/test/groovy/com/studioreservation/domain/reservation/service/ReservationServiceTest.groovy
+++ src/test/groovy/com/studioreservation/domain/reservation/service/ReservationServiceTest.groovy
@@ -2,12 +2,15 @@ package com.studioreservation.domain.reservation.service
 
 import com.studioreservation.domain.calendar.service.CalendarService
 import com.studioreservation.domain.featuretoggle.service.FeatureToggleService
+import com.studioreservation.domain.reservation.dto.ReservationRequestDTO
 import com.studioreservation.domain.reservation.dto.ReservationResponseDTO
 import com.studioreservation.domain.reservation.entity.ReservationHistory
 import com.studioreservation.domain.reservation.enums.PayTyp
 import com.studioreservation.domain.reservation.enums.ReservationState
 import com.studioreservation.domain.reservation.mapper.ReservationMapper
 import com.studioreservation.domain.reservation.repository.ReservationRepository
+import com.studioreservation.domain.room.entity.Room
+import com.studioreservation.domain.room.enums.RoomType
 import com.studioreservation.domain.room.repository.RoomRepository
 import com.studioreservation.global.request.PageRequestDTO
 import org.springframework.data.domain.PageImpl
@@ -27,8 +30,10 @@ class ReservationServiceTest extends Specification {
     private ReservationMapper reservationMapper = Mock()
     private CalendarService calendarService = Mock()
     private PageRequestDTO pageRequestDTO
+    private ReservationRequestDTO requestDTO
     private ReservationHistory reservationHistory
     private ReservationResponseDTO responseDTO
+    private Room room
 
     def setup() {
         sut = new ReservationService(featureToggleService,
@@ -66,6 +71,39 @@ class ReservationServiceTest extends Specification {
         .proposal("test")
         .build()
 
+        requestDTO = ReservationRequestDTO.builder()
+        .senderNm("test")
+        .needTaxInvoce(true)
+        .requestCont("test")
+        .policyConfirmed(true)
+        .proposal("test")
+        .phone("01050578403")
+        .strtDt(Timestamp.valueOf(LocalDateTime.now()))
+        .endDt(Timestamp.valueOf(LocalDateTime.now()))
+        .build()
+
+        room = Room.builder()
+                .cd(1L)
+                .capacity(2)
+                .name("A")
+                .description("A 룸")
+                .dayPrice(44000)
+                .halfHrPrice(22000)
+                .roomType(RoomType.NORMAL)
+                .useYn(true)
+                .build()
+
+        reservationHistory = ReservationHistory.builder()
+                .senderNm("test")
+                .needTaxInvoce(true)
+                .requestCont("test")
+                .policyConfirmed(true)
+                .proposal("test")
+                .phone("01050578403")
+                .strtDt(Timestamp.valueOf(LocalDateTime.now()))
+                .endDt(Timestamp.valueOf(LocalDateTime.now()))
+                .build()
+
     }
 
     def "모든 예약 정보 받아오기"() {
@@ -78,5 +116,19 @@ class ReservationServiceTest extends Specification {
 
         then:
         result.getDtoList().size() == 1
+    }
+
+    def "예약하기"() {
+        given:
+        Long roomCd = 1L
+        roomRepository.findSingleEntity(roomCd) >> room
+        reservationMapper.toEntity(requestDTO) >> reservationHistory
+        reservationMapper.toDTO(reservationHistory) >> responseDTO
+
+        when:
+        def result = sut.reserve(roomCd, requestDTO)
+
+        then:
+        result.getPhone() == "01050578403"
     }
 }
