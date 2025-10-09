8a78fa8eab4e863064cce2ae148d66ee7dadf0fb
Gwon Junhyeok <jh10253267@gmail.com>
2025-10-09 13:45:00 +0900

    feature(#114): 유효성 검사 실패시 필드와 이유 리턴하도록 수정

--- src/main/java/com/studioreservation/global/exception/GlobalControllerAdvice.java
+++ src/main/java/com/studioreservation/global/exception/GlobalControllerAdvice.java
@@ -3,12 +3,16 @@ package com.studioreservation.global.exception;
 import static com.studioreservation.global.exception.ErrorCode.*;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import com.studioreservation.global.response.APIResponse;
 
 import lombok.extern.slf4j.Slf4j;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 @RestControllerAdvice
@@ -26,4 +30,20 @@ public class GlobalControllerAdvice {
 		return ResponseEntity.status(DATABASE_ERROR.getStatus())
 			.body(APIResponse.error(DATABASE_ERROR.name()));
 	}
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> validationErrorHandler(MethodArgumentNotValidException e) {
+        log.error("Validation error: {}", e.toString());
+
+        Map<String, String> errors = new HashMap<>();
+        e.getBindingResult().getFieldErrors().forEach(error ->
+                errors.put(error.getField(), error.getDefaultMessage())
+        );
+
+        return ResponseEntity.badRequest()
+                .body(APIResponse.error(errors));
+    }
+
+
+
 }
