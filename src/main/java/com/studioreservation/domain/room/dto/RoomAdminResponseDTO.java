3b938291a95481ce93c2be44825dda42a21662df
Gwon Junhyeok <jh10253267@gmail.com>
2025-10-12 18:57:18 +0900

    feature(#125): 관리자가 useYn을 볼 수 있도록 수정

---
+++ src/main/java/com/studioreservation/domain/room/dto/RoomAdminResponseDTO.java
@@ -0,0 +1,42 @@
+package com.studioreservation.domain.room.dto;
+
+import com.studioreservation.domain.room.enums.RoomType;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RoomAdminResponseDTO {
+    @Schema(description = "방 식별 번호", example = "방 식별 번호")
+    private Long cd;
+
+    @Schema(description = "방 이름", example = "방 이름")
+    private String name;
+
+    @Schema(description = "30분당 가격", example = "30분당 가격")
+    private int halfHrPrice;
+
+    @Schema(description = "일별 가격", example = "일별 가격")
+    private int dayPrice;
+
+    @Schema(description = "최소 대여 시간", example = "최소 대여 시간")
+    private int minTm;
+
+    @Schema(description = "수용 가능 인원", example = "수용 가능 인원")
+    private int capacity;
+
+    @Schema(description = "방 호칭", example = "방 호칭")
+    private String title;
+
+    @Schema(description = "방 설명", example = "방 설명")
+    private String description;
+
+    @Schema(description = "방 타입", example = "방 타입")
+    private RoomType roomType;
+
+    @Schema(description = "예약 가능 여부", example = "예약 가능 여부")
+    private boolean useYn;
+}
