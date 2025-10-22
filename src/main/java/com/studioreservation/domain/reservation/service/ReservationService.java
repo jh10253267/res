35bd913af90b5edac65ed54adf122d03d69b023b
Gwon Junhyeok <jh10253267@gmail.com>
2025-10-22 18:41:23 +0900

    feature(#135) : 수정

--- src/main/java/com/studioreservation/domain/reservation/service/ReservationService.java
+++ src/main/java/com/studioreservation/domain/reservation/service/ReservationService.java
@@ -76,6 +76,8 @@ public class ReservationService {
     public void adminReserve(ReservationChangeRequestDTO requestDTO) {
         ReservationHistory reservationHistory = mapper.toEntity(requestDTO);
         reservationHistory.setRoomInfo(roomInfoRepository.findSingleEntity(requestDTO.getRoomCd()));
+        reservationHistory.setPlatform(platformRepository.findSingleEntity(requestDTO.getPlatformCd()));
+
         repository.save(reservationHistory);
     }
 
