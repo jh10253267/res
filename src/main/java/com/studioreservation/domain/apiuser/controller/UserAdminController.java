98c18e1982f47a9a26ea512d1c429f88c798938e
Gwon Junhyeok <jh10253267@gmail.com>
2025-10-29 19:22:58 +0900

    feature(#145): 회원가입 어드민으로 전환

--- src/main/java/com/studioreservation/domain/apiuser/controller/UserAdminController.java
+++ src/main/java/com/studioreservation/domain/apiuser/controller/UserAdminController.java
@@ -1,52 +1,26 @@
 package com.studioreservation.domain.apiuser.controller;
 
-import com.studioreservation.domain.apiuser.dto.RefreshTokenDTO;
-import org.springframework.http.ResponseEntity;
-
 import com.studioreservation.domain.apiuser.dto.SignupRequestDTO;
 import com.studioreservation.domain.apiuser.service.UserService;
 import com.studioreservation.global.response.APIResponse;
-
-import org.springframework.web.bind.annotation.*;
-
-import com.studioreservation.domain.apiuser.dto.LoginRequestDTO;
-
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/users")
+@RequestMapping("/api/admin/users")
 public class UserAdminController {
-	private final UserService service;
+    private final UserService service;
 
-	@GetMapping("/healthCheck")
-	public String healthCheck() {
-		return "OK";
-	}
-	@Operation(summary = "회원가입", description = "사용자 회원가입 API")
-	@PostMapping("/signup")
-	public APIResponse<?> signup(@RequestBody SignupRequestDTO signupRequestDTO) {
-		service.signup(signupRequestDTO);
+    @Operation(summary = "회원가입", description = "사용자 회원가입 API")
+    @PostMapping("/signup")
+    public APIResponse<?> signup(@RequestBody SignupRequestDTO signupRequestDTO) {
+        service.signup(signupRequestDTO);
 
-		return APIResponse.success();
-	}
-
-	@Operation(summary = "로그인", description = "사용자 로그인 API")
-	@ApiResponses(value = {
-			@ApiResponse(responseCode = "200", description = "로그인 성공"),
-			@ApiResponse(responseCode = "401", description = "인증 실패")
-	})
-	@PostMapping("/login")
-	public ResponseEntity<String> login(@RequestBody LoginRequestDTO loginRequest) {
-		return ResponseEntity.ok("로그인은 필터에서 처리됩니다.");
-	}
-
-	@Operation(summary = "토큰 리프레싱", description = "토큰 리프레싱 API")
-	@PostMapping("/refreshToken")
-	public ResponseEntity<String> refreshToken(@RequestBody RefreshTokenDTO refreshTokenDTO) {
-		return ResponseEntity.ok("리프레싱은 필터에서 처리됩니다.");
-	}
+        return APIResponse.success();
+    }
 }
